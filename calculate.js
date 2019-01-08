cef570f420f6f22232759f564289cf75c192693a
Dominic Tremblay <dominictremblay24@gmail.com>
2019-01-08 10:34:12 -0500

    conversion of arguments into numbers

--- calculate.js
+++ calculate.js
@@ -4,7 +4,18 @@
 // and output the results.
 
 // Extract the arguments from the command-line
-// numbers <- arguments
+// Remove the first 2 elements of process.argv
+var args = process.argv.slice(2);
+console.log('args: ', args);
+
+var numbers = [];
+
+// Convert the strings into actual numbers
+for (var i = 0; i < args.length; i++) {
+  var nb = Number(args[i]);
+  numbers.push(nb);
+}
+
 // Total <- 0
 
 // iterate over the numbers and add each to total
