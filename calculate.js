15a3253b6d7efea243035b5dd81413acecddd8de
Dominic Tremblay <dominictremblay24@gmail.com>
2019-01-08 09:50:12 -0500

    wrote the pseudo-code of the program

---
+++ calculate.js
@@ -0,0 +1,12 @@
+// Write a node program that takes an
+// unlimited amount of arguments from
+// the command-line and adds them together
+// and output the results.
+
+// Extract the arguments from the command-line
+// numbers <- arguments
+// Total <- 0
+
+// iterate over the numbers and add each to total
+
+// Output the result
