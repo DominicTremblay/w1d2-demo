6a7b7a851d44849f9d927abe4dee33b7550a52ed
Dominic Tremblay <dominictremblay24@gmail.com>
2019-01-08 13:39:28 -0500

    added the error message when arguments is NaN

--- calculate.js
+++ calculate.js
@@ -5,19 +5,46 @@
 
 // Extract the arguments from the command-line
 // Remove the first 2 elements of process.argv
-var args = process.argv.slice(2);
-console.log('args: ', args);
 
-var numbers = [];
-
-// Convert the strings into actual numbers
-for (var i = 0; i < args.length; i++) {
-  var nb = Number(args[i]);
-  numbers.push(nb);
+function getArguments(args) {
+  return process.argv.slice(2);
 }
 
-// Total <- 0
+function validateArgs(numbers) {
+  numbers.forEach(function(nb) {
+    if (isNaN(nb)) {
+      console.log('Please input only numbers');
+      process.exit();
+    }
+  });
+}
 
-// iterate over the numbers and add each to total
+function convertToNumbers(args) {
+  var numbers = [];
+  // Convert the strings into actual numbers
+  for (var i = 0; i < args.length; i++) {
+    var nb = Number(args[i]);
+    numbers.push(nb);
+  }
+  return numbers;
+}
 
+function sum(numbers) {
+  // Total <- 0
+  var total = 0;
+
+  // iterate over the numbers and add each to total
+  for (var i = 0; i < numbers.length; i++) {
+    total = total + numbers[i];
+  }
+  return total;
+}
+
+var args = getArguments(process.argv);
+var numbers = convertToNumbers(args);
+console.log('Numbers: ', numbers);
+validateArgs(numbers);
+
+var total = sum(numbers);
 // Output the result
+console.log('Total: ', total);
